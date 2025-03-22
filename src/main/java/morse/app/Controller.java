8c9f336d72e8d2ee74b63cc504095cc118fb8bdf
Askemanden <askevandsoaremark@gmail.com>
2025-03-22 20:40:32 +0100

    added controller FXML id'ed objects

--- src/main/java/morse/app/Controller.java
+++ src/main/java/morse/app/Controller.java
@@ -5,16 +5,46 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 
 public class Controller implements Initializable {
 
     @FXML
-    
+    VBox buttonContainer;
+    @FXML
+    Button translatorButton;
+    @FXML
+    Button settingsButton;
+    @FXML
+    Button exitButton;
+    @FXML
+    VBox translator;
+    @FXML
+    TextArea inputTextArea;
+    @FXML
+    TextArea outputTextArea;
+    @FXML
+    VBox settings;
+    @FXML
+    VBox messageBox;
+    @FXML
+    Label chatName;
+    @FXML
+    ScrollPane messages;
+    @FXML
+    VBox messagesContainer;
+    @FXML
+    VBox profile;
+    @FXML
+    Label username;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+        System.out.println("tester");
     }
     
     
